c192b289fe1c0a96277768ce83c1e992c5f93a32
ongaro09 <ongaromark7@gmail.com>
2024-05-06 05:06:50 +0300

    BnB clone index file

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,36 @@
 #!/usr/bin/python3
-from api.v1.views import app_views
+"""BnB clone index file"""
+
 from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.place import Place
+from models.city import City
+from models.review import Review
+from models.user import User
+from models.state import State
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    class_list = {
+        "User": "users",
+        "City": "cities",
+        "Amenity": "amenities",
+        "Review": "reviews",
+        "Place": "places",
+        "State": "states",
+    }
+    cl_list = ["User", "City", "Amenity", "Place", "State", "Review"]
+    count_dict = {}
+
+    for class_name in class_list.keys():
+        count_dict[class_list[class_name]] = storage.count(class_name)
+
+    return jsonify(count_dict)
