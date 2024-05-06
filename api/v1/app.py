a05e83561a46493cbf8f96b9bd7c6ca649cebcc9
ongaro09 <ongaromark7@gmail.com>
2024-05-06 04:59:02 +0300

    the config file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
-"""
-This is a Flask App
-"""
-import os
-from flask import Flask, make_response, jsonify
+"""Configuration file"""
+
+from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
-
+import os
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """
-    Close the storage
-    """
+def teardown_appcont(self):
+    """this is where the database session ends"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_reached(error):
+    """This function handles pages that do not exist"""
+    e = {
+        "error": "Not Found"
+    }
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
+    ports = os.getenv("HBNB_API_PORT", "5000")
+    app.run(host=host, port=ports, threaded=True)
