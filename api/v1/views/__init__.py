d77be293c84f27e61fc929ff6a2adf1f3eb62207
ongaro09 <ongaromark7@gmail.com>
2024-05-06 05:37:13 +0300

    Flask Blueprint for handling RESTful API actions

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-
+"""Flask Blueprint for handling RESTful API actions"""
 from flask import Blueprint
 
-# Create a Blueprint instance with url prefix /api/v1
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Wildcard import of everything in the package api.v1.views.index
+app_views = Blueprint("app_views", __name__, url_prefix='/api/v1')
+
+
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
